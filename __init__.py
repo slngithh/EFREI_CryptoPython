5aa865a11ad58bf5d640749d34749f4287d6ccaf
slngithh <solene.crebrb@outlook.fr>
2024-12-03 10:59:51 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ import sqlite3
 app = Flask(__name__)                                                                                                                 
                                                                                                                                        
 @app.route('/')
-def hello_world():
+def hello_world(): 
     return render_template('hello.html')
 
 key = Fernet.generate_key()
