b2ed6f54d5f626fe6787e671ac15ad4fd30db73f
slngithh <solene.crebrb@outlook.fr>
2024-12-03 10:48:24 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask import json
 from urllib.request import urlopen
 import sqlite3 
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__)                                                                                                                 
                                                                                                                                        
 @app.route('/')
 def hello_world():
