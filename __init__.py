027faebae020244be01476845252cf77f7f80587
slngithh <solene.crebrb@outlook.fr>
2025-01-07 10:21:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,27 +12,16 @@ def exercice1():
     return render_template('exercice1.html')
 
 @app.route('/exercice2')
-def hello_world(): 
+def exercice2(): 
     return render_template('exercice2.html')
 
 @app.route('/exercice3')
-def hello_world(): 
+def exercice3(): 
     return render_template('exercice3.html')
 
 key = Fernet.generate_key()
 f = Fernet(key)
 
-@app.route('/encrypt/<string:valeur>')
-def encryptage(valeur):
-    valeur_bytes = valeur.encode()  # Conversion str -> bytes
-    token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-
-@app.route('/decrypt/<string:token_encrypted>')
-def decryptage(token_encrypted):
-    valeur_decryptee = f.decrypt(token_bytes).decode()  # Décoder en str
-    token_bytes = token_encrypted.encode()
-    return f"Valeur décryptée : {valeur_decryptee}"
 
 
 if __name__ == "__main__":
